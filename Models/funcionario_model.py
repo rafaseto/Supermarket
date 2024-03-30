15778ac7e6921ab5d3cca22375726990e1f53c6e
Rafael Takeguma <rafael08.0807@gmail.com>
2024-03-29 23:22:27 -0300

    implementa classe Funcionario em funcionario_model.py

--- Models/funcionario_model.py
+++ Models/funcionario_model.py
@@ -1,1 +1,27 @@
- 
+class Funcionario:
+    def __init__(self,
+                 nome,
+                 cpf,
+                 data_nascimento,
+                 email,
+                 telefone,
+                 sexo,
+                 login,
+                 senha,
+                 status):
+        self.id = None
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
+        self.email = email
+        self.telefone = telefone
+        self.sexo = sexo
+        self.login = login
+        self.senha = senha
+        self.status = status
+
+    def __str__(self):
+        return (f"Nome: {self.nome} / "
+                f"CPF: {self.cpf} / "
+                f"Telefone: {self.telefone} / " 
+                f"Status: {self.status}")
