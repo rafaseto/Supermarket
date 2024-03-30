f420a8d4b57dfb4ef475efcc48f9a8677525e3eb
Rafael Takeguma <rafael08.0807@gmail.com>
2024-03-29 23:30:28 -0300

    implementa classe FuncionarioView em funcionario_view.py

--- Views/funcionario_view.py
+++ Views/funcionario_view.py
@@ -1,1 +1,12 @@
- 
+class FuncionarioView:
+    def mostrar_detalhes(self, funcionario):
+        print("Detalhes do funcionário:")
+        print("------------------------")
+        print(f"Nome: {funcionario.nome}")
+        print(f"CPF: {funcionario.cpf}")
+        print(f"Data de nascimento: {funcionario.data_nascimento}")
+        print(f"E-mail: {funcionario.email}")
+        print(f"Telefone: {funcionario.telefone}")
+        print(f"Sexo: {funcionario.sexo}")
+        print(f"Login: {funcionario.login}")
+        print(f"Status: {funcionario.status}") 
